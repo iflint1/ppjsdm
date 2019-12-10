b17ce75129c03df985916e39a3ab44e4ebda3cd8
Ian Flint <iflint@6300l-152366-l.mobility.unimelb.edu.au>
2019-12-11 09:20:36 +1100

    Wrote script to test inference procedure -- current values point to error somewhere.

---
+++ scripts/new_inference_tests.R
@@ -0,0 +1,10 @@
+alpha <- 0.67
+log_lambda <- 3.6
+nsim <- 1000
+radius <- 0.1
+model <- "Geyer"
+
+Z <- rmultigibbs(Rectangle_window(), matrix(alpha), exp(log_lambda), nsim = nsim, radius = radius, model = model)
+W <- lapply(Z, function(z) coef(gibbsm(z, Rectangle_window(), model = model, radius = radius, print = FALSE)))
+cat("Estimated values are: ", paste0(Reduce("+", W) / length(W), collapse = ", "), ".\n", sep = "")
+cat("True values are: log_lambda = ", log_lambda, " and alpha = ", alpha, ".\n", sep = "")
