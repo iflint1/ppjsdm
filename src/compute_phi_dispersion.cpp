31999bab7942df83d99ee0044ecbf87de7906d5d
Ian Flint <iflint@6300l-837210-l.mobility.unimelb.net.au>
2019-12-18 17:11:24 +1100

    Small efficiency improvement.

--- src/compute_phi_dispersion.cpp
+++ src/compute_phi_dispersion.cpp
@@ -15,24 +15,25 @@
 //' @import Rcpp
 // [[Rcpp::export]]
 [[nodiscard]] Rcpp::NumericVector compute_delta_phi_dispersion(Rcpp::S4 configuration, Rcpp::NumericVector location, R_xlen_t type, int number_types, Rcpp::CharacterVector model = "identity", double radius = 0) {
+  const auto x{Rcpp::NumericVector(configuration.slot("x"))};
   if(model[0] == "identity") {
     const auto varphi{Varphi_model_papangelou<varphi::Identity>{}};
-    return varphi.compute(Rcpp::NumericVector(configuration.slot("x")),
+    return varphi.compute(x,
                           Rcpp::NumericVector(configuration.slot("y")),
                           Rcpp::IntegerVector(configuration.slot("types")),
-                          location, type, number_types);
+                          location, type, number_types, x.size());
   } else if(model[0] == "Strauss") {
     const auto varphi{Varphi_model_papangelou<varphi::Strauss>{radius}};
-    return varphi.compute(Rcpp::NumericVector(configuration.slot("x")),
+    return varphi.compute(x,
                           Rcpp::NumericVector(configuration.slot("y")),
                           Rcpp::IntegerVector(configuration.slot("types")),
-                          location, type, number_types);
+                          location, type, number_types, x.size());
   } else if(model[0] == "Geyer"){
     const auto varphi{Geyer_papangelou{radius, 2.0}};
-    return varphi.compute(Rcpp::NumericVector(configuration.slot("x")),
+    return varphi.compute(x,
                           Rcpp::NumericVector(configuration.slot("y")),
                           Rcpp::IntegerVector(configuration.slot("types")),
-                          location, type, number_types);
+                          location, type, number_types, x.size());
   } else {
     Rcpp::stop("Incorrect model entered.\n");
     return {};
--- src/compute_phi_dispersion.h
+++ src/compute_phi_dispersion.h
@@ -7,60 +7,6 @@
 #include <limits> // std::numeric_limits
 
 #include "compute_phi_distance.h"
-// #include "configuration_utilities.h"
-
-// template<typename V, typename X, typename Y, typename T>
-// [[nodiscard]] inline Rcpp::NumericMatrix compute_phi_dispersion_helper(const X& x, const Y& y, const T& types_vector, int number_types, const V& varphi) {
-//   // TODO: Might be able to avoid recomputing this every time, marginal efficiency gain.
-//   const R_xlen_t number_points{x.size()};
-//
-//   // dispersion and count_species are automatically 0-initialized
-//   Rcpp::NumericMatrix dispersion(number_types, number_types);
-//   Rcpp::IntegerVector count_species(number_types);
-//
-//   for(R_xlen_t i{0}; i < number_points; ++i) {
-//     const auto type_i{types_vector[i] - 1};
-//     ++count_species[type_i];
-//
-//     for(R_xlen_t j{i + 1}; j < number_points; ++j) {
-//       const auto type_j{types_vector[j] - 1};
-//
-//       const auto phi_distance{compute_phi_distance(x[i], y[i], x[j], y[j], varphi)};
-//
-//       dispersion(type_i, type_j) += phi_distance;
-//       if(type_i != type_j) {
-//         dispersion(type_j, type_i) += phi_distance;
-//       }
-//     }
-//   }
-//
-//   for(int i{0}; i < number_types; ++i) {
-//     const auto count_species_i{count_species[i]};
-//     if(count_species_i > 0) {
-//       if(count_species_i != 1) {
-//         dispersion(i, i) *= 2. / (count_species_i * (count_species_i - 1));
-//       }
-//       for(int j{i + 1}; j < number_types; ++j) {
-//         const auto count_species_j{count_species[j]};
-//         if(count_species_j > 0) {
-//           dispersion(i, j) /= count_species_i * count_species_j;
-//           dispersion(j, i) /= count_species_i * count_species_j;
-//         }
-//       }
-//     }
-//   }
-//
-//   return dispersion;
-// }
-//
-// template<typename V>
-// [[nodiscard]] inline Rcpp::NumericMatrix compute_phi_dispersion_helper(Rcpp::S4 configuration, int number_types, const V& varphi) {
-//   return compute_phi_dispersion_helper(Rcpp::NumericVector(configuration.slot("x")),
-//                                        Rcpp::NumericVector(configuration.slot("y")),
-//                                        Rcpp::IntegerVector(configuration.slot("types")),
-//                                        number_types,
-//                                        varphi);
-// }
 
 template<typename Varphi>
 class Varphi_model_papangelou: public Varphi {
@@ -68,10 +14,7 @@ public:
   template<typename... Args>
   Varphi_model_papangelou(Args&&... args): Varphi{std::forward<Args>(args)...} {}
   template<typename X, typename Y, typename T>
-  [[nodiscard]] inline Rcpp::NumericVector compute(const X& x, const Y& y, const T& types_vector, Rcpp::NumericVector location, R_xlen_t type, R_xlen_t number_types) const {
-    // TODO: Might be able to avoid recomputing this every time, marginal efficiency gain.
-    const auto number_points = R_xlen_t(x.size());
-
+  [[nodiscard]] inline Rcpp::NumericVector compute(const X& x, const Y& y, const T& types_vector, Rcpp::NumericVector location, R_xlen_t type, R_xlen_t number_types, R_xlen_t number_points) const {
     R_xlen_t same_type_points{0};
     for(R_xlen_t i{0}; i < number_points; ++i) {
       const auto type_i{types_vector[i] - 1};
@@ -125,25 +68,11 @@ public:
   }
 };
 
-// template<typename V>
-// [[nodiscard]] inline Rcpp::NumericVector compute_delta_phi_dispersion_helper(Rcpp::S4 configuration, Rcpp::NumericVector location, R_xlen_t type, R_xlen_t number_types, const V& varphi) {
-//   return compute_delta_phi_dispersion_helper(Rcpp::NumericVector(configuration.slot("x")),
-//                                           Rcpp::NumericVector(configuration.slot("y")),
-//                                           Rcpp::IntegerVector(configuration.slot("types")),
-//                                           location,
-//                                           type,
-//                                           number_types,
-//                                           varphi);
-// }
-
 class Geyer_papangelou {
 public:
   Geyer_papangelou(double radius, double saturation) noexcept: square_radius_{radius * radius}, saturation_{saturation} {}
   template<typename X, typename Y, typename T>
-  [[nodiscard]] inline Rcpp::NumericVector compute(const X& x, const Y& y, const T& types_vector, Rcpp::NumericVector location, R_xlen_t type, R_xlen_t number_types) const {
-    // TODO: Might be able to avoid recomputing this every time, marginal efficiency gain.
-    const auto number_points = R_xlen_t(x.size());
-
+  [[nodiscard]] inline Rcpp::NumericVector compute(const X& x, const Y& y, const T& types_vector, Rcpp::NumericVector location, R_xlen_t type, R_xlen_t number_types, R_xlen_t number_points) const {
     Rcpp::NumericVector delta_dispersion(number_types);
     for(R_xlen_t i{0}; i < number_points; ++i) {
       const auto type_i{types_vector[i] - 1};
@@ -172,13 +101,13 @@ public:
   Nearest_neighbour_papangelou(Args&&... args): Varphi{std::forward<Args>(args)...} {}
 
   template<typename X, typename Y, typename T>
-  [[nodiscard]] inline Rcpp::NumericVector compute(const X& x, const Y& y, const T& types_vector, Rcpp::NumericVector location, R_xlen_t type, R_xlen_t number_types) const {
+  [[nodiscard]] inline Rcpp::NumericVector compute(const X& x, const Y& y, const T& types_vector, Rcpp::NumericVector location, R_xlen_t type, R_xlen_t number_types, R_xlen_t number_points) const {
     // TODO: Might want to not initialize
     Rcpp::NumericVector delta_dispersion(number_types);
 
     for(R_xlen_t i{0}; i < number_types; ++i) {
       if(i == type) {
-        const auto d{-compute_dispersion(x, y, types_vector, i, i, number_types)};
+        const auto d{-compute_dispersion(x, y, types_vector, i, i, number_types, number_points)};
         auto copy_x{x};
         auto copy_y{y};
         auto copy_types{types_vector};
@@ -186,10 +115,10 @@ public:
         copy_y.push_back(location[1]);
         copy_types.push_back(type + 1);
 
-        delta_dispersion[i] = d + compute_dispersion(copy_x, copy_y, copy_types, i, i, number_types);
+        delta_dispersion[i] = d + compute_dispersion(copy_x, copy_y, copy_types, i, i, number_types, number_points);
       } else {
-        auto d{-compute_dispersion(x, y, types_vector, i, type, number_types)};
-        d -= compute_dispersion(x, y, types_vector, type, i, number_types);
+        auto d{-compute_dispersion(x, y, types_vector, i, type, number_types, number_points)};
+        d -= compute_dispersion(x, y, types_vector, type, i, number_types, number_points);
         auto copy_x{x};
         auto copy_y{y};
         auto copy_types{types_vector};
@@ -197,7 +126,7 @@ public:
         copy_y.push_back(location[1]);
         copy_types.push_back(type + 1);
 
-        delta_dispersion[i] = 0.5 * (d + compute_dispersion(copy_x, copy_y, copy_types, i, type, number_types) + compute_dispersion(copy_x, copy_y, copy_types, type, i, number_types));
+        delta_dispersion[i] = 0.5 * (d + compute_dispersion(copy_x, copy_y, copy_types, i, type, number_types, number_points) + compute_dispersion(copy_x, copy_y, copy_types, type, i, number_types, number_points));
       }
     }
 
@@ -208,10 +137,8 @@ private:
   [[nodiscard]] inline double compute_dispersion(const X& x, const Y& y,
                                                  const T& types_vector,
                                                  R_xlen_t k1, R_xlen_t k2,
-                                                 R_xlen_t number_types) const {
-    // TODO: Might be able to avoid recomputing this every time, marginal efficiency gain.
-    const auto number_points = R_xlen_t(x.size());
-
+                                                 R_xlen_t number_types,
+                                                 R_xlen_t number_points) const {
     // count_species automatically 0-initialized
     R_xlen_t count_species;
     double dispersion{0};
@@ -258,7 +185,7 @@ public:
   Exponential_family_model(const U& lambda, const V& alpha, Args&&... args): Varphi(std::forward<Args>(args)...), lambda_{lambda}, alpha_{alpha} {}
   template<typename X, typename Y, typename T>
   [[nodiscard]] double compute_papangelou(const X& x, const Y& y, const T& types_vector, Rcpp::NumericVector location, R_xlen_t type, R_xlen_t number_types) const {
-    const auto delta_D{Varphi::compute(x, y, types_vector, location, type, number_types)};
+    const auto delta_D{Varphi::compute(x, y, types_vector, location, type, number_types, x.size())};
 
     double inner_product{0};
     for(R_xlen_t i{0}; i < number_types; ++i) {
