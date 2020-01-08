52ee7d3a951f07c4abd949049090887eabaf2e2a
Ian Flint <iflint@6300l-154784-l.mobility.unimelb.net.au>
2020-01-08 15:02:56 +1100

    rbinomialpp_single.h C++14 conformance.

--- src/rbinomialpp_single.h
+++ src/rbinomialpp_single.h
@@ -5,17 +5,16 @@
 
 #include "configuration_utilities.h"
 #include "sample_from_window.h"
-#include "window_utilities.h"
 
-template<Window WindowType>
-[[nodiscard]] inline Rcpp::List rbinomialpp_single(const Window_wrapper<WindowType>& window, Rcpp::IntegerVector n, Rcpp::CharacterVector types, R_xlen_t point_types, R_xlen_t total_number) {
-  auto x{Rcpp::NumericVector(Rcpp::no_init(total_number))};
-  auto y{Rcpp::NumericVector(Rcpp::no_init(total_number))};
-  auto factors{Rcpp::IntegerVector(Rcpp::no_init(total_number))};
+template<typename W>
+[[nodiscard]] inline Rcpp::List rbinomialpp_single(const W& window, Rcpp::IntegerVector n, Rcpp::CharacterVector types, R_xlen_t point_types, R_xlen_t total_number) {
+  Rcpp::NumericVector x(Rcpp::no_init(total_number));
+  Rcpp::NumericVector y(Rcpp::no_init(total_number));
+  Rcpp::IntegerVector factors(Rcpp::no_init(total_number));
 
-  int fill{0};
-  for(R_xlen_t j{0}; j < point_types; ++j) {
-    const auto points_to_add{n[j]};
+  int fill(0);
+  for(R_xlen_t j(0); j < point_types; ++j) {
+    const auto points_to_add(n[j]);
     sample_from_window(window, x, y, factors, points_to_add, fill, j + 1);
     fill += points_to_add;
   }
