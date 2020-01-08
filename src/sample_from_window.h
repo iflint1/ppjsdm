f7d2b2e83b2d2fdcacd86069a6756f518f015caf
Ian Flint <iflint@6300l-154784-l.mobility.unimelb.net.au>
2020-01-08 17:41:36 +1100

    Fixed brackets in sample_from_window.h and made try to remove error when compiling on Windows.

--- src/sample_from_window.h
+++ src/sample_from_window.h
@@ -7,8 +7,8 @@ namespace ppjsdm {
 
 template<typename S, typename T, typename U, typename V>
 inline void sample_from_window(const S& window, T& x, U& y, V& types, R_xlen_t additions, R_xlen_t filling, int types_value) {
-  for(R_xlen_t i{0}; i < additions; ++i) {
-    const auto sample{window.sample()};
+  for(R_xlen_t i(0); i < additions; ++i) {
+    const auto sample(window.sample());
     x[filling + i] = sample.first;
     y[filling + i] = sample.second;
     types[filling + i] = types_value;
