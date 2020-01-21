a634a5e82d5d91df6060287a1917e531da292ec6
Ian Flint <ian.flint@unimelb.edu.au>
2020-01-21 17:04:10 +1100

    Cleaned up gibbsm and the vignette.

--- R/gibbsm.R
+++ R/gibbsm.R
@@ -40,58 +40,54 @@ gibbsm <- function(configuration_list, window = Rectangle_window(), covariates =
   gibbsm_data_list <- lapply(configuration_list, function(configuration) prepare_gibbsm_data(configuration, window, covariates, model, radius))
   glm_fits <- lapply(gibbsm_data_list, function(gibbsm_data) glm(as.formula(gibbsm_data$formula), data = as.data.frame(gibbsm_data$data), family = binomial()))
 
-  number_of_individuals_by_configuration <- lapply(configuration_list, function(configuration) get_number_points(configuration, total = FALSE))
-  species_names_by_configuration <- lapply(number_of_individuals_by_configuration, function(n) names(n))
-  number_of_species_by_configuration <- lapply(number_of_individuals_by_configuration, function(n) length(n))
   number_configurations <- length(configuration_list)
-  number_traits <- length(traits)
-  number_of_alpha_coefficients_by_configuration <- lapply(number_of_species_by_configuration, function(n) n * (n + 1) / 2)
-  regression_length <- Reduce("+", number_of_alpha_coefficients_by_configuration)
-  response <- vector(mode = "numeric", length = regression_length)
-  joint_regressors <- matrix(data = NA, nrow = regression_length, ncol = number_traits)
-  #additive_regressors <- matrix(data = NA, nrow = regression_length, ncol = number_traits)
 
-  # TODO: Move to C++?
-  index <- 1
-  for(i in seq_len(number_configurations)) {
-    for(j in seq_len(number_of_species_by_configuration[[i]])) {
-      current_name_j <- species_names_by_configuration[[i]][j]
-      for(k in j:number_of_species_by_configuration[[i]]) {
-        alpha_string <- paste0("alpha", "_", j, "_", k)
-        response[index] <- coefficients(glm_fits[[i]])[alpha_string]
-        current_name_k <- species_names_by_configuration[[i]][k]
-        joint_regressors[index, ] <- sapply(traits, function(trait) trait[current_name_j] * trait[current_name_k] / (trait[current_name_j] + trait[current_name_k]))
-        #additive_regressors[index, ] <- sapply(traits, function(trait) trait[current_name_j] + trait[current_name_k])
-        index <- index + 1
+  # If traits have been given, proceed to fit our submodel for alpha ~ traits.
+  number_traits <- length(traits)
+  if(number_traits > 0) {
+    number_of_individuals_by_configuration <- lapply(configuration_list, function(configuration) get_number_points(configuration, total = FALSE))
+    species_names_by_configuration <- lapply(number_of_individuals_by_configuration, function(n) names(n))
+    number_of_species_by_configuration <- lapply(number_of_individuals_by_configuration, function(n) length(n))
+    number_of_alpha_coefficients_by_configuration <- lapply(number_of_species_by_configuration, function(n) n * (n + 1) / 2)
+    regression_length <- Reduce("+", number_of_alpha_coefficients_by_configuration)
+    response <- vector(mode = "numeric", length = regression_length)
+    joint_regressors <- matrix(data = NA, nrow = regression_length, ncol = number_traits)
+
+    # TODO: Move to C++?
+    index <- 1
+    for(i in seq_len(number_configurations)) {
+      for(j in seq_len(number_of_species_by_configuration[[i]])) {
+        current_name_j <- species_names_by_configuration[[i]][j]
+        for(k in j:number_of_species_by_configuration[[i]]) {
+          alpha_string <- paste0("alpha", "_", j, "_", k)
+          response[index] <- coefficients(glm_fits[[i]])[alpha_string]
+          current_name_k <- species_names_by_configuration[[i]][k]
+          joint_regressors[index, ] <- sapply(traits, function(trait) trait[current_name_j] * trait[current_name_k] / (trait[current_name_j] + trait[current_name_k]))
+          index <- index + 1
+        }
       }
     }
-  }
 
-  if(number_traits > 0) {
     colnames(joint_regressors) <- paste0("joint_", names(traits))
-    #colnames(additive_regressors) <- paste0("additive_", names(traits))
-    #formula <- paste0("response ~ 1 + ", paste(colnames(joint_regressors), collapse = " + "),
-    #                  " + ", paste(colnames(additive_regressors), collapse = " + "))
-    # g <- lm(as.formula(formula), data = data.frame(response, joint_regressors, additive_regressors))
     formula <- paste0("response ~ 1 + ", paste(colnames(joint_regressors), collapse = " + "))
-    g <- lm(as.formula(formula), data = data.frame(response, joint_regressors))
+    traits_fit <- lm(as.formula(formula), data = data.frame(response, joint_regressors))
   }
 
   if(print) {
     lapply(glm_fits, function(fit) print(coefficients(fit)))
     if(number_traits > 0) {
-      print(coefficients(g))
+      print(coefficients(traits_fit))
     }
   }
 
   if(number_configurations > 1) {
-    f <- glm_fits
+    fits <- glm_fits
   } else {
-    f <- glm_fits[[1]]
+    fits <- glm_fits[[1]]
   }
   if(number_traits > 0) {
-    list(complete = f, traits = g)
+    list(complete = fits, traits = traits_fit)
   } else {
-    f
+    fits
   }
 }
