cd0c411c5f8f4e6e78859c0df5ce1002bcd00343
Ian Flint <ian.flint@unimelb.edu.au>
2020-01-22 21:27:58 +1100

    Tweaks in make_default_types.

--- src/utility/make_default_types.h
+++ src/utility/make_default_types.h
@@ -5,7 +5,6 @@
 #include <Rinternals.h>
 
 #include <string> // std::string, std::to_string
-#include <utility> // std::forward
 
 namespace ppjsdm {
 namespace detail {
@@ -15,22 +14,23 @@ inline Rcpp::CharacterVector get_best_names(R_xlen_t size) {
 }
 
 template<typename... Args>
-inline Rcpp::CharacterVector get_best_names(R_xlen_t size, SEXP might_contain_names, Args&&... other) {
+inline Rcpp::CharacterVector get_best_names(R_xlen_t size, SEXP might_contain_names, Args... others) {
   const SEXP potential_names(RCPP_GET_NAMES(might_contain_names));
   if(Rf_isNull(potential_names)) {
-    return get_best_names(size, std::forward<Args>(other)...);
+    return get_best_names(size, others...);
   } else {
     return Rcpp::as<Rcpp::CharacterVector>(potential_names);
   }
 }
 
 template<typename... Args>
-inline SEXP make_default_types(R_xlen_t size, Args&&... might_contain_names) {
-  Rcpp::CharacterVector given_names(get_best_names(size, std::forward<Args>(might_contain_names)...));
+inline SEXP make_default_types(R_xlen_t size, Args... might_contain_names) {
+  Rcpp::CharacterVector given_names(get_best_names(size, might_contain_names...));
   Rcpp::CharacterVector default_types(Rcpp::no_init(size));
   for(R_xlen_t i(0); i < size; ++i) {
-    if(given_names[i] != "") {
-      default_types[i] = given_names[i];
+    const auto current_name(given_names[i]);
+    if(current_name != "") {
+      default_types[i] = current_name;
     } else {
       default_types[i] = std::string("type").append(std::to_string(i + 1));
     }
@@ -42,19 +42,21 @@ inline SEXP make_default_types(R_xlen_t size, Args&&... might_contain_names) {
 } // namespace detail
 
 template<typename... Args>
-inline SEXP make_types(SEXP types, R_xlen_t size, Args&&... might_contain_names) {
+inline SEXP make_types(SEXP types, R_xlen_t size, Args... might_contain_names) {
   if(Rf_isNull(types)) {
-    return detail::make_default_types(size, std::forward<Args>(might_contain_names)...);
+    return detail::make_default_types(size, might_contain_names...);
   } else if (Rf_isNewList(types)) {
-    Rcpp::List list(types);
+    const auto list(Rcpp::as<Rcpp::List>(types));
     const auto length_types(list.size());
     Rcpp::CharacterVector new_types(length_types);
     for(R_xlen_t i(0); i < length_types; ++i) {
       new_types[i] =  Rf_asChar(list[i]);
     }
     return Rcpp::wrap(new_types);
-  } else {
+  } else if (Rf_isVector(types)) {
     return types;
+  } else {
+    Rcpp::stop("The type of `types` is not convertible to a character vector.");
   }
 }
 
