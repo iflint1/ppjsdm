78a8c96002c50d477e89b813419cdead4898f181
Ian Flint <ian.flint@unimelb.edu.au>
2020-01-24 10:04:04 +1100

    Split gibbsm benchmark.

---
+++ benchmarks/gibbsm_with_covariate.R
@@ -0,0 +1,24 @@
+library(microbenchmark)
+library(ppjsdm)
+library(spatstat)
+
+window <- Rectangle_window()
+window_spatstat <- owin()
+
+temperature <- function(x, y) x + y
+
+configuration_spatstat <- rmpoispp(lambda = list(a = function(x, y) exp(4 + temperature(x, y))), win = window_spatstat)
+configuration <- as.Configuration(configuration_spatstat)
+
+plot(configuration_spatstat)
+
+set.seed(42)
+
+b <- microbenchmark(
+  "ppjsdm::gibbsm" = gibbsm(configuration, window = window, covariates = list(temperature), print = FALSE),
+  # `logi` is the fastest method according to docs, but `mpl` appears to be faster here.
+  "spatstat::ppm" = ppm(configuration_spatstat ~ 1 + temperature, method = "mpl"),
+  times = 100L
+)
+
+print(b)
