4cae43fef6f1835918c897cab668749796fcf6a6
Ian Flint <ian.flint@unimelb.edu.au>
2020-02-16 17:49:07 +1100

    Wider range of values for rHardcore benchmark.

--- benchmarks/rHardcore.R
+++ benchmarks/rHardcore.R
@@ -7,10 +7,10 @@ window_spatstat <- owin()
 
 set.seed(42)
 
-b <- microbenchmark(
-  "ppjsdm" = rgibbs(window = window, lambda = 25, alpha = -Inf, saturation = Inf, model = "Geyer", short_range = 0.1),
-  "spatstat" = rHardcore(25, R = 0.1, W = window_spatstat, expand = FALSE),
-  times = 10000L
-)
+values <- list(c(25, 0.15), c(90, 0.08), c(15, 0.06), c(55, 0.06))
 
-print(b)
+b <- lapply(values, function(v) microbenchmark(
+            "ppjsdm" = rgibbs(window = window, lambda = v[1], short_range = v[2], alpha = -Inf, saturation = Inf, model = "Geyer"),
+            "spatstat" = rHardcore(v[1], R = v[2], W = window_spatstat, expand = FALSE), times = 1000L))
+
+lapply(b, function(a) print(a))
