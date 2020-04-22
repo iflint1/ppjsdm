a774a5458500031c2a40216f2fcf1252778bcbd0
Ian Flint <ian.flint@unimelb.edu.au>
2020-04-22 12:40:59 +1000

    More inference tests.

--- scripts/inference_tests.R
+++ scripts/inference_tests.R
@@ -1,15 +1,67 @@
-alpha <- 0
-log_lambda <- 3.6
-nsim <- 100
-radius <- 0.1
-model <- "neighbour"
-steps <- 100000
+library(ppjsdm)
+
+alpha <- matrix(-0.1)
+gamma <- matrix(0)
+lambda <- 30
+nsim <- 1000
+short_range <- matrix(0.1)
+medium_range <- matrix(0)
+long_range <- matrix(0)
+model <- "square_exponential"
+medium_range_model <- "square_exponential"
+steps <- 0
 window_side <- c(0, 1)
 window <- Rectangle_window(window_side, window_side)
+use_rgibbs <- TRUE
+use_joint_pdf <- TRUE
 
-Z <- rgibbs(window, matrix(alpha), exp(log_lambda), nsim = nsim, radius = radius, model = model, steps = steps)
-#Z <- rppp(window, exp(log_lambda), nsim = nsim)
-G <-lapply(Z, function(z) gibbsm(z, window, model = model, radius = radius, print = FALSE))
-W <- lapply(G, function(g) coef(g))
-cat("Estimated values are: ", paste0(Reduce("+", W) / length(W), collapse = ", "), ".\n", sep = "")
-cat("True values are: log_lambda = ", log_lambda, " and alpha = ", alpha, ".\n", sep = "")
+if(use_rgibbs) {
+  Z <- ppjsdm::rgibbs(window = window,
+                      alpha = alpha,
+                      gamma = gamma,
+                      lambda = lambda,
+                      nsim = nsim,
+                      short_range = short_range,
+                      medium_range = medium_range,
+                      long_range = long_range,
+                      model = model,
+                      medium_range_model = medium_range_model,
+                      steps = steps,
+                      drop = FALSE)
+} else {
+  Z <- ppjsdm::rppp(window = window,
+                    lambda = lambda,
+                    nsim = nsim,
+                    drop = FALSE)
+}
+
+if(use_joint_pdf) {
+  G <-gibbsm(Z,
+             window = window,
+             model = model,
+             medium_range_model = medium_range_model,
+             short_range = short_range,
+             medium_range = medium_range,
+             long_range = long_range,
+             print = FALSE,
+             use_glmnet = FALSE)
+  estimate <- G$coefficients
+} else {
+  G <-lapply(Z, function(z) gibbsm(z,
+                                   window = window,
+                                   model = model,
+                                   medium_range_model = medium_range_model,
+                                   short_range = short_range,
+                                   medium_range = medium_range,
+                                   long_range = long_range,
+                                   print = FALSE,
+                                   use_glmnet = FALSE))
+  W <- lapply(G, function(g) g$coefficients)
+  if(length(W) == 1) {
+    estimate <- unlist(W)
+  } else {
+    estimate <- Reduce(function(a, b) unlist(a) + unlist(b), W) / length(W)
+  }
+}
+cat("Estimated values are: ", paste0(estimate, collapse = ", "), ".\n", sep = "")
+cat("True values are: lambda = ", lambda, " and alpha = ", alpha, ".\n", sep = "")
