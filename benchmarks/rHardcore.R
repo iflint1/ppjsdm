099ae78dc72da1dad19d5962a7e93bfee6bd6e4f
Ian Flint <ian.flint@unimelb.edu.au>
2020-04-28 11:41:59 +1000

    Added gamma parameter to the benchmarks.

--- benchmarks/rHardcore.R
+++ benchmarks/rHardcore.R
@@ -10,7 +10,7 @@ set.seed(42)
 values <- list(c(25, 0.15), c(90, 0.08), c(15, 0.06), c(55, 0.06))
 
 b <- lapply(values, function(v) microbenchmark(
-            "ppjsdm" = rgibbs(window = window, lambda = v[1], short_range = v[2], alpha = -Inf, saturation = Inf, model = "Geyer"),
+            "ppjsdm" = rgibbs(window = window, lambda = v[1], short_range = v[2], alpha = -Inf, gamma = 0, saturation = Inf, model = "Geyer"),
             "spatstat" = rHardcore(v[1], R = v[2], W = window_spatstat, expand = FALSE), times = 1000L))
 
 lapply(b, function(a) print(a))
