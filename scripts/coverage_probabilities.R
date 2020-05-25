e6b476ee50b5be4946c87b44a9565334ea5158a7
Ian Flint <ian.flint@unimelb.edu.au>
2020-05-25 17:05:14 +1000

    Improve coverage probabilities script.

--- scripts/coverage_probabilities.R
+++ scripts/coverage_probabilities.R
@@ -7,13 +7,9 @@ window <- Rectangle_window(c(-1, 1), c(-1, 1))
 nreplications <- 1
 ntypes <- 2
 beta0 <- rep(2, ntypes)
-estimate_gamma <- FALSE
+estimate_gamma <- TRUE
 alpha <- cbind(c(-0.3, 0.1), c(0.1, -0.2))
-if(estimate_gamma) {
-  gamma <- cbind(c(-0.5, -0.2), c(-0.2, -0.3))
-} else {
-  gamma <- matrix(0., 2, 2)
-}
+
 covariates <- list(temperature = function(x, y) x, elevation = function(x, y) y)
 beta <- cbind(rep(2, ntypes), rep(1, ntypes))
 ndummy <- 500
@@ -26,13 +22,12 @@ short_range <- matrix(0.1, ntypes, ntypes)
 medium_range <- matrix(0.1, ntypes, ntypes)
 if(estimate_gamma) {
   long_range <- matrix(0.15, ntypes, ntypes)
+  is_in <- matrix(NA, nrow = nreplications, ncol = ntypes + ntypes * (ntypes + 1) + length(covariates) * ntypes)
+  gamma <- cbind(c(-0.5, -0.2), c(-0.2, -0.3))
 } else {
   long_range <- matrix(0.1, ntypes, ntypes)
-}
-if(estimate_gamma) {
-  is_in <- matrix(NA, nrow = nreplications, ncol = ntypes + ntypes * (ntypes + 1) + length(covariates) * ntypes)
-} else {
   is_in <- matrix(NA, nrow = nreplications, ncol = ntypes + ntypes * (ntypes + 1) / 2 + length(covariates) * ntypes)
+  gamma <- matrix(0., ntypes, ntypes)
 }
 estimates <- matrix(NA, nrow = nreplications, ncol = ncol(is_in))
 true <- c(beta0, alpha[1, 1], alpha[1, 2], alpha[2, 2])
@@ -43,11 +38,6 @@ if(estimate_gamma) {
 if(length(covariates) > 0) {
   true <- c(true, beta[1, 1], beta[2, 1], beta[1, 2], beta[2, 2])
 }
-
-# samples <- ppjsdm::rppp(window = window,
-#                         lambda = exp(beta0),
-#                         nsim = nreplications,
-#                         drop = FALSE)
 
 samples <- ppjsdm::rgibbs(window = window,
                           beta0 = beta0,
@@ -63,13 +53,6 @@ samples <- ppjsdm::rgibbs(window = window,
                           saturation = saturation,
                           covariates = covariates,
                           beta = beta)
-
-# samples <- spatstat::rStrauss(beta = exp(beta0),
-#                               gamma = exp(2 * alpha),
-#                               R = short_range, W = as.owin(window),
-#                               nsim = nreplications,
-#                               drop = FALSE)
-# samples <- lapply(samples, function(s) ppjsdm::Configuration(x = s$x, y = s$y))
 
 
 for(i in seq_len(nreplications)) {
