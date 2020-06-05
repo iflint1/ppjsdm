0218fa13800dea879ab6ed02e2c6929c983b8ec8
Ian Flint <ian.flint@unimelb.edu.au>
2020-06-05 13:21:24 +1000

    Remove separate file for coerce_to_named_im_objects

--- R/model_parameters.R
+++ R/model_parameters.R
@@ -1,3 +1,17 @@
+add_names <- function(str, covariates) {
+  if(is.null(names(covariates))) {
+    no_name <- rep(TRUE, length(covariates))
+  } else {
+    no_name <- names(covariates) == ""
+  }
+  names(covariates)[no_name] <- sprintf(paste0(str, "%d"), seq_len(length(which(no_name))))
+  covariates
+}
+
+coerce_to_named_im_objects <- function(lst, str, window) {
+  lst <- lapply(as.list(lst), function(element) as.im(element, W = as.owin(window)))
+  add_names(str, lst)
+}
 
 model_parameters_defaults <- function(window,
                                       covariates,
