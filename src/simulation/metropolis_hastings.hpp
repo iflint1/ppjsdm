37031c97bedb3c1c3adf917393888af3ce491cb0
Ian Flint <ian.flint@unimelb.edu.au>
2021-04-08 15:00:15 +1000

    Cleaned up Metropolis-Hastings some more, commented out another (equivalent) version

--- src/simulation/metropolis_hastings.hpp
+++ src/simulation/metropolis_hastings.hpp
@@ -13,15 +13,15 @@ namespace ppjsdm {
 template<typename Configuration, typename Model>
 inline auto simulate_metropolis_hastings(const Model& model, const Window& window, unsigned long long int steps, R_xlen_t number_types) {
   // Start from a rough approximate draw and go from there.
-  const auto points(approximate_draw<Configuration>(model));
+  const auto starting_configuration(approximate_draw<Configuration>(model));
 
-  return simulate_metropolis_hastings(model, window, steps, number_types, points);
+  return simulate_metropolis_hastings(model, window, steps, number_types, starting_configuration);
 }
 
 template<typename Configuration, typename Model>
 inline auto simulate_metropolis_hastings(const Model& model, const Window& window, unsigned long long int steps, R_xlen_t number_types, const Configuration& starting_configuration) {
   constexpr double birth_probability(0.5);
-  const double precomputed_constant((1 - birth_probability) * window.volume() * static_cast<double>(number_types) / birth_probability);
+  const double precomputed_constant((1 - birth_probability) / birth_probability * window.volume() * static_cast<double>(number_types));
 
   Configuration points(starting_configuration);
 
@@ -30,12 +30,11 @@ inline auto simulate_metropolis_hastings(const Model& model, const Window& windo
       const R_xlen_t random_type(Rcpp::sample(number_types, 1, false, R_NilValue, false)[0]);
       const auto point(window.sample(random_type));
 
-      // TODO: You can avoid taking the exp by reorganising the ratio, and sampling an exponential r.v. instead.
       const double papangelou(model.compute_papangelou(point, points));
       const double birth_ratio(papangelou * precomputed_constant / (1. + static_cast<double>(size(points))));
 
       // Use C++ short-circuiting
-      if(birth_ratio >= 1. || unif_rand() <= birth_ratio) {
+      if(birth_ratio >= 1. || birth_ratio >= unif_rand()) {
         add_point(points, point);
       }
     } else if(!empty(points)) {
@@ -46,11 +45,36 @@ inline auto simulate_metropolis_hastings(const Model& model, const Window& windo
       double death_ratio(current_size / (precomputed_constant * papangelou));
 
       // Use C++ short-circuiting
-      if(death_ratio < 1. && unif_rand() > death_ratio) {
+      if(death_ratio <= 1. && death_ratio <= unif_rand()) {
         add_point(points, point);
       }
     }
   }
+  // for(decltype(steps) k(0); k < steps; ++k) {
+  //   if(unif_rand() <= birth_probability) {
+  //     const R_xlen_t random_type(Rcpp::sample(number_types, 1, false, R_NilValue, false)[0]);
+  //     const auto point(window.sample(random_type));
+  //
+  //     const double log_papangelou(model.compute_log_papangelou(point, points));
+  //     const double log_birth_ratio(log_papangelou + precomputed_constant - std::log(1. + static_cast<double>(size(points))));
+  //
+  //     // Use C++ short-circuiting
+  //     if(log_birth_ratio >= 0. || log_birth_ratio + exp_rand() >= 0) {
+  //       add_point(points, point);
+  //     }
+  //   } else if(!empty(points)) {
+  //     const double current_size(size(points));
+  //     const auto point(remove_random_point(points));
+  //
+  //     const double log_papangelou(model.compute_log_papangelou(point, points));
+  //     const double log_death_ratio(log_papangelou + precomputed_constant - std::log(current_size));
+  //
+  //     // Use C++ short-circuiting
+  //     if(log_death_ratio >= 0. && log_death_ratio >= exp_rand()) {
+  //       add_point(points, point);
+  //     }
+  //   }
+  // }
   return points;
 }
 
