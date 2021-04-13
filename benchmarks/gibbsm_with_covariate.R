735301814ccb49634dbbea658af85e08dedd9a21
Ian Flint <ian.flint@unimelb.edu.au>
2021-04-13 10:45:31 +1000

    Added a few other options to gibbsm benchmark

--- benchmarks/gibbsm_with_covariate.R
+++ benchmarks/gibbsm_with_covariate.R
@@ -19,7 +19,8 @@ nd <- 100
 b <- microbenchmark(
   "ppjsdm::gibbsm" = gibbsm(configuration, covariates = list(temperature = temperature), use_glmnet = FALSE, short_range = 0, dummy_factor = 1e6, max_dummy = nd * nd, print = FALSE),
   # `logi` is the fastest method according to docs, but `mpl` appears to be faster here.
-  "spatstat::ppm" = ppm(configuration_spatstat ~ 1 + temperature, method = "mpl", nd = c(nd, nd)),
+  "spatstat::ppm mpl" = ppm(configuration_spatstat ~ 1 + temperature, method = "mpl", nd = c(nd, nd)),
+  "spatstat::ppm logi" = ppm(configuration_spatstat ~ 1 + temperature, method = "logi", nd = c(nd, nd)),
   times = 100L
 )
 
