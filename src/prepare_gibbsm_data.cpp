5be3369fe30a5edbb323847829c3589705b1c45e
Ian Flint <ian.flint@unimelb.edu.au>
2021-04-19 14:36:01 +1000

    Remove parallel section in gibbsm; not needed anymore!

--- src/prepare_gibbsm_data.cpp
+++ src/prepare_gibbsm_data.cpp
@@ -97,118 +97,29 @@ Rcpp::List prepare_gibbsm_data_helper(const std::vector<Configuration>& configur
     shift[j] = -std::log(static_cast<double>(rho_times_volume[j]) / volume);
   }
 
-  // Precompute dispersion and other computation-intensive things.
-  struct Result_type {
-    Result_type(): is_in_configuration(), type(), dispersion(), medium_dispersion(), covariates() {}
-
-    Result_type(bool i, int t, std::vector<double> v, std::vector<double> w, std::vector<double> x, double new_x, double new_y, double m):
-      is_in_configuration(i), type(t), dispersion(std::move(v)), medium_dispersion(std::move(w)), covariates(std::move(x)), x(new_x), y(new_y), mark(m) {}
-
-    bool is_in_configuration;
-    int type;
-    std::vector<double> dispersion;
-    std::vector<double> medium_dispersion;
-    std::vector<double> covariates;
-    double x;
-    double y;
-    double mark;
-  };
-  std::vector<Result_type> precomputed_results;
-  unsigned long long int total_configuration_length(0);
-  for(size_t i(0); i < configuration_list.size(); ++i) {
-    total_configuration_length += ppjsdm::size(configuration_list[i]);
-  }
-  precomputed_results.reserve(total_configuration_length + length_D * configuration_list.size());
-
-  // TODO: Trying to precompute a lot of things, write better explanations
+  // Precompute short and medium range dispersions
   std::vector<std::vector<std::vector<double>>> dispersion_short(configuration_list.size());
   std::vector<std::vector<std::vector<double>>> dispersion_medium(configuration_list.size());
   for(size_t configuration_index(0); configuration_index < configuration_list.size(); ++configuration_index) {
     if(need_to_compute_alpha) {
       dispersion_short[configuration_index] = ppjsdm::compute_dispersion_index(dispersion_model,
                                                                                number_types,
-                                                                               configuration_list[configuration_index],
-                                                                               D);
+                                                                               configuration_list[configuration_index], D);
     }
     if(need_to_compute_gamma) {
       dispersion_medium[configuration_index] = ppjsdm::compute_dispersion_index(medium_dispersion_model,
                                                                                 number_types,
-                                                                                configuration_list[configuration_index],
-                                                                                D);
+                                                                                configuration_list[configuration_index], D);
     }
   }
 
-  const size_t covariates_length(covariates.size());
-#pragma omp parallel
-  {
-  std::vector<Result_type> results_private;
-  results_private.reserve(total_configuration_length + length_D * configuration_list.size());
-#pragma omp for nowait
-  for(size_t i = 0; i < total_configuration_length; ++i) {
-    size_t configuration_index(0);
-    unsigned long long int previous_count(0);
-    while(i >= previous_count + ppjsdm::size(configuration_list[configuration_index])) {
-      previous_count += ppjsdm::size(configuration_list[configuration_index]);
-      ++configuration_index;
-    }
-    // configuration_index contains the index of the configuration we're at in the loop.
-    const auto point_index(i - previous_count);
-    std::vector<double> d;
-    if(need_to_compute_alpha) {
-      d = dispersion_short[configuration_index][point_index];
-      // d = ppjsdm::compute_dispersion(dispersion_model, configuration_list[configuration_index][point_index], number_types, configuration_list[configuration_index]);
-    }
-    std::vector<double> e;
-    if(need_to_compute_gamma) {
-      e = dispersion_medium[configuration_index][point_index];
-      // e = ppjsdm::compute_dispersion(medium_dispersion_model, configuration_list[configuration_index][point_index], number_types, configuration_list[configuration_index]);
-    }
-    std::vector<double> cov(covariates_length);
-    for(size_t k(0); k < covariates_length; ++k) {
-      const auto covariate(covariates[k](configuration_list[configuration_index][point_index]));
-      if(R_IsNA(covariate)) {
-        Rcpp::stop("One of the covariates' value is NA on one of the locations in the dataset.");
-      }
-      cov[k] = covariate;
-    }
-    results_private.emplace_back(true, ppjsdm::get_type(configuration_list[configuration_index][point_index]), std::move(d), std::move(e), std::move(cov), ppjsdm::get_x(configuration_list[configuration_index][point_index]), ppjsdm::get_y(configuration_list[configuration_index][point_index]), ppjsdm::get_mark(configuration_list[configuration_index][point_index]));
-  }
-#pragma omp for nowait
-  for(size_t i = 0; i < length_D; ++i) {
-    bool one_of_covariates_na(false);
-    std::vector<double> cov(covariates_length);
-    for(size_t k(0); k < covariates_length; ++k) {
-      const auto covariate(covariates[k](D[i]));
-      if(R_IsNA(covariate)) {
-        one_of_covariates_na = true;
-        break;
-      }
-      cov[k] = covariate;
-    }
-    // Get rid of locations with an NA value for one of the covariates.
-    if(one_of_covariates_na) {
-      continue;
-    }
-    for(size_t j(0); j < configuration_list.size(); ++j) {
-      std::vector<double> d;
-      if(need_to_compute_alpha) {
-        d = dispersion_short[j][i + ppjsdm::size(configuration_list[j])];
-        // d = ppjsdm::compute_dispersion(dispersion_model, D[i], number_types, configuration_list[j]);
-      }
-      std::vector<double> e;
-      if(need_to_compute_gamma) {
-        e = dispersion_medium[j][i + ppjsdm::size(configuration_list[j])];
-        // e = ppjsdm::compute_dispersion(medium_dispersion_model, D[i], number_types, configuration_list[j]);
-      }
-      results_private.emplace_back(false, ppjsdm::get_type(D[i]), std::move(d), std::move(e), cov, ppjsdm::get_x(D[i]), ppjsdm::get_y(D[i]), ppjsdm::get_mark(D[i]));
-    }
-  }
-#pragma omp critical
-  precomputed_results.insert(precomputed_results.end(), results_private.begin(), results_private.end());
+  // Compue a few parameters necessary for the construction of regressors
+  unsigned long long int total_configuration_length(0);
+  for(size_t i(0); i < configuration_list.size(); ++i) {
+    total_configuration_length += ppjsdm::size(configuration_list[i]);
   }
 
-  //const size_t number_traits(traits.size());
-  const auto total_points(precomputed_results.size());
+  const auto total_points(total_configuration_length + length_D * configuration_list.size());
   const auto number_parameters_struct(ppjsdm::get_number_parameters(number_types,
                                                                     covariates.size(),
                                                                     estimate_alpha,
@@ -227,87 +138,80 @@ Rcpp::List prepare_gibbsm_data_helper(const std::vector<Configuration>& configur
   Rcpp::NumericMatrix regressors(total_points, total_parameters);
 
   // Fill the regressors, response, offset and shift with what we precomputed.
-  for(size_t i(0); i < total_points; ++i) {
-    if(precomputed_results[i].is_in_configuration) {
-      response[i] = 1;
-    }
-    x[i] = precomputed_results[i].x;
-    y[i] = precomputed_results[i].y;
-    mark[i] = precomputed_results[i].mark;
+  size_t filling(0);
+  for(size_t configuration_index(0); configuration_index < configuration_list.size(); ++configuration_index) {
+    for(size_t point_index(0); point_index < ppjsdm::size(configuration_list[configuration_index]) + ppjsdm::size(D); ++point_index) {
+      const auto current_point(point_index < ppjsdm::size(configuration_list[configuration_index]) ?
+                                 configuration_list[configuration_index][point_index] :
+                                 D[point_index - ppjsdm::size(configuration_list[configuration_index])]);
+      if(point_index < ppjsdm::size(configuration_list[configuration_index])) {
+        response[filling] = 1;
+      }
+      x[filling] = ppjsdm::get_x(current_point);
+      y[filling] = ppjsdm::get_y(current_point);
+      mark[filling] = ppjsdm::get_mark(current_point);
 
-    const int type_index(precomputed_results[i].type);
-    type[i] = type_index + 1;
-
-    rho_offset[i] = -std::log(static_cast<double>(rho_times_volume[type_index]) / volume);
-
-    // fill traits
-    // short_range_traits_input(i, 0) = precomputed_results[i].dispersion[type_index];
-    // medium_range_traits_input(i, 0) = precomputed_results[i].medium_dispersion[type_index];
-    // for(int j(0); j < number_types; ++j) {
-    //   if(j != type_index) {
-    //     short_range_joint_traits_input(i, 0) += precomputed_results[i].dispersion[j];
-    //     medium_range_joint_traits_input(i, 0) += precomputed_results[i].medium_dispersion[j];
-    //   }
-    // }
-    //
-    // for(size_t k(0); k < number_traits; ++k) {
-    //   short_range_traits_input(i, k + 1) = Rcpp::as<Rcpp::NumericVector>(traits[k])[type_index] * precomputed_results[i].dispersion[type_index];
-    //   medium_range_traits_input(i, k + 1) = Rcpp::as<Rcpp::NumericVector>(traits[k])[type_index] * precomputed_results[i].medium_dispersion[type_index];
-    //   for(int j(0); j < number_types; ++j) {
-    //     if(j != type_index) {
-    //       const auto delta(std::abs(Rcpp::as<Rcpp::NumericVector>(traits[k])[j] - Rcpp::as<Rcpp::NumericVector>(traits[k])[type_index]));
-    //       short_range_joint_traits_input(i, k + 1) += delta * precomputed_results[i].dispersion[j];
-    //       medium_range_joint_traits_input(i, k + 1) += delta * precomputed_results[i].medium_dispersion[j];
-    //     }
-    //   }
-    // }
-
-    size_t index_alpha(0);
-    size_t index_gamma(0);
-    for(int j(0); j < number_types; ++j) {
-      if(j == type_index) {
-        // fill log_lambda
-        regressors(i, j) = 1.;
-
-        // fill covariates
-        for(size_t k(0); k < covariates_length; ++k) {
-          regressors(i, index_start_covariates + k * number_types + j) = precomputed_results[i].covariates[k];
+      std::vector<double> covariates_values(covariates.size());
+      for(size_t covariate_index(0); covariate_index < static_cast<size_t>(covariates.size()); ++covariate_index) {
+        const auto covariate(covariates[covariate_index](configuration_list[configuration_index][point_index]));
+        if(R_IsNA(covariate)) {
+          Rcpp::stop("One of the covariates' value is NA on one of the locations in the dataset.");
         }
+        covariates_values[covariate_index] = covariate;
+      }
 
-        // fill alpha & gamma
-        for(int k(j); k < number_types; ++k) {
-          if(estimate_alpha(j, k)) {
-            regressors(i, number_types + index_alpha) = precomputed_results[i].dispersion[k];
-            ++index_alpha;
+      const int type_index(ppjsdm::get_type(current_point));
+      type[filling] = type_index + 1;
+
+      rho_offset[filling] = -std::log(static_cast<double>(rho_times_volume[type_index]) / volume);
+
+      size_t index_alpha(0);
+      size_t index_gamma(0);
+      for(int j(0); j < number_types; ++j) {
+        if(j == type_index) {
+          // fill log_lambda
+          regressors(filling, j) = 1.;
+
+          // fill covariates
+          for(size_t covariate_index(0); covariate_index < static_cast<size_t>(covariates.size()); ++covariate_index) {
+            regressors(filling, index_start_covariates + covariate_index * number_types + j) = covariates_values[covariate_index];
           }
-          if(estimate_gamma(j, k)) {
-            regressors(i, index_start_gamma + index_gamma) = precomputed_results[i].medium_dispersion[k];
-            ++index_gamma;
-          }
-        }
-      } else {
-        // fill alpha & gamma
-        for(int k(j); k < number_types; ++k) {
-          if(estimate_alpha(j, k)) {
-            if(k == type_index) {
-              regressors(i, number_types + index_alpha) = precomputed_results[i].dispersion[j];
+
+          // fill alpha & gamma
+          for(int k(j); k < number_types; ++k) {
+            if(estimate_alpha(j, k)) {
+              regressors(filling, number_types + index_alpha) = dispersion_short[configuration_index][point_index][k];
+              ++index_alpha;
             }
-            ++index_alpha;
-          }
-          if(estimate_gamma(j, k)) {
-            if(k == type_index) {
-              regressors(i, index_start_gamma + index_gamma) = precomputed_results[i].medium_dispersion[j];
+            if(estimate_gamma(j, k)) {
+              regressors(filling, index_start_gamma + index_gamma) = dispersion_medium[configuration_index][point_index][k];
+              ++index_gamma;
             }
-            ++index_gamma;
+          }
+        } else {
+          // fill alpha & gamma
+          for(int k(j); k < number_types; ++k) {
+            if(estimate_alpha(j, k)) {
+              if(k == type_index) {
+                regressors(filling, number_types + index_alpha) = dispersion_short[configuration_index][point_index][j];
+              }
+              ++index_alpha;
+            }
+            if(estimate_gamma(j, k)) {
+              if(k == type_index) {
+                regressors(filling, index_start_gamma + index_gamma) = dispersion_medium[configuration_index][point_index][j];
+              }
+              ++index_gamma;
+            }
           }
         }
       }
+      ++filling;
     }
   }
 
   const auto col_names(make_model_coloumn_names(covariates, number_types, estimate_alpha, estimate_gamma));
   Rcpp::colnames(regressors) = col_names;
-
 
   return Rcpp::List::create(Rcpp::Named("response") = response,
                             Rcpp::Named("x") = x,
@@ -353,15 +257,11 @@ Rcpp::List prepare_gibbsm_data(Rcpp::List configuration_list,
 
   auto max_points_by_type(ppjsdm::get_number_points(vector_configurations[0]));
   const auto number_types(max_points_by_type.size());
-  //auto max_points_in_any_type(*std::max_element(max_points_by_type.begin(), max_points_by_type.end()));
 
   using size_t = typename decltype(vector_configurations)::size_type;
   for(size_t i(0); i < vector_configurations.size(); ++i) {
     const auto new_points_by_type(ppjsdm::get_number_points(vector_configurations[i]));
     for(size_t j(0); j < max_points_by_type.size(); ++j) {
-      // if(max_points_in_any_type < new_points_by_type[j]) {
-      //   max_points_in_any_type = new_points_by_type[j];
-      // }
       if(max_points_by_type[j] < new_points_by_type[j]) {
         max_points_by_type[j] = new_points_by_type[j];
       }
