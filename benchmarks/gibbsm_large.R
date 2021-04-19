b7a6ced45e10d07985855f1cc07dedd4616fef63
Ian Flint <ian.flint@unimelb.edu.au>
2021-04-19 17:06:15 +1000

    Fairer comparison with Strauss process

--- benchmarks/gibbsm_large.R
+++ benchmarks/gibbsm_large.R
@@ -11,6 +11,7 @@ y_covariate <- function(x, y) y
 dummy_factor <- 1e6
 model <- "Geyer"
 medium_range_model <- "Geyer"
+saturation <- Inf
 
 set.seed(42)
 
@@ -31,6 +32,7 @@ b <- microbenchmark(
                                                                       dummy_factor = dummy_factor,
                                                                       max_dummy = nd * nd,
                                                                       model = model,
+                                                                      saturation = saturation,
                                                                       medium_range_model = medium_range_model),
   "ppjsdm::gibbsm with short-range" = ppjsdm::gibbsm(configuration,
                                                      covariates = list(x_covariate = x_covariate, y_covariate = y_covariate),
@@ -39,6 +41,7 @@ b <- microbenchmark(
                                                      dummy_factor = dummy_factor,
                                                      max_dummy = nd * nd,
                                                      model = model,
+                                                     saturation = saturation,
                                                      medium_range_model = medium_range_model),
   "ppjsdm::gibbsm without ranges" = ppjsdm::gibbsm(configuration,
                                                    covariates = list(x_covariate = x_covariate, y_covariate = y_covariate),
@@ -47,6 +50,7 @@ b <- microbenchmark(
                                                    dummy_factor = dummy_factor,
                                                    max_dummy = nd * nd,
                                                    model = model,
+                                                   saturation = saturation,
                                                    medium_range_model = medium_range_model),
   # `logi` is the fastest method according to docs, but `mpl` appears to be faster here.
   "spatstat::ppm Poisson mpl" = ppm(configuration_spatstat ~ 1 + x_covariate + y_covariate, method = "mpl", nd = c(nd, nd)),
