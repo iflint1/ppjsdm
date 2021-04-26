f1798ecb25bd8200ceca77f829ae5c6ba4171bae
Ian Flint <ian.flint@unimelb.edu.au>
2021-04-27 09:33:28 +1000

    Prepare for quicker Papangelou computation

--- src/saturated_model/compute_dispersion_fitting.hpp
+++ src/saturated_model/compute_dispersion_fitting.hpp
@@ -18,6 +18,7 @@ struct dispersion_computation_fitting {
   template<typename Configuration, typename OtherConfiguration>
   auto operator()(const Saturated_model& varphi,
                 R_xlen_t number_types,
+                bool compute_on_configuration,
                 const Configuration& configuration,
                 const OtherConfiguration& other_configuration) const {
     using ValueType = typename AbstractDispersion::ValueType;
@@ -28,7 +29,8 @@ struct dispersion_computation_fitting {
     using size_t = std::remove_cv_t<decltype(size(configuration))>;
     std::vector<CountType> count_vector(configuration_size);
     const auto other_configuration_size(size(other_configuration));
-    std::vector<DispersionType> dispersion(configuration_size + other_configuration_size);
+    const auto index_configuration(compute_on_configuration ? configuration_size : 0);
+    std::vector<DispersionType> dispersion(index_configuration + other_configuration_size);
 
     // TODO: The code below seems to work, but it can perhaps be optimized or grouped together with the non-saturated
     // case below.
@@ -44,19 +46,21 @@ struct dispersion_computation_fitting {
         }
       }
 
-      for(size_t i(0); i < configuration_size; ++i) {
-        dispersion[i] = DispersionType(number_types);
-        add_count_to_dispersion<0, AbstractDispersion, 2>(varphi, dispersion[i], count_vector[i], configuration[i]);
+      if(compute_on_configuration) {
+        for(size_t i(0); i < configuration_size; ++i) {
+          dispersion[i] = DispersionType(number_types);
+          add_count_to_dispersion<0, AbstractDispersion, 2>(varphi, dispersion[i], count_vector[i], configuration[i]);
+        }
       }
 
       for(size_t i(0); i < other_configuration_size; ++i) {
-        dispersion[configuration_size + i] = DispersionType(number_types);
+        dispersion[index_configuration + i] = DispersionType(number_types);
         CountType count_point(number_types);
         for(size_t j(0); j < configuration_size; ++j) {
           AbstractDispersion::template update_count<std::numeric_limits<int>::max()>(varphi, count_point[get_type(configuration[j])],
                                                                                   other_configuration[i], configuration[j]);
         }
-        add_count_to_dispersion<0, AbstractDispersion, 2>(varphi, dispersion[configuration_size + i],
+        add_count_to_dispersion<0, AbstractDispersion, 2>(varphi, dispersion[index_configuration + i],
                                                           count_point, other_configuration[i]);
       }
     } else {
@@ -69,24 +73,26 @@ struct dispersion_computation_fitting {
         }
       }
 
-      for(size_t i(0); i < configuration_size; ++i) {
-        dispersion[i] = DispersionType(number_types);
-        for(size_t j(0); j < i; ++j) {
-          // TODO: varphi(configuration[i], configuration[j]) only needs to be computed once
-          dispersion[i][get_type(configuration[j])] += AbstractDispersion::template delta<1, true>(varphi, count_vector[j][get_type(configuration[i])], configuration[j], configuration[i]);
-          dispersion[j][get_type(configuration[i])] += AbstractDispersion::template delta<1, true>(varphi, count_vector[i][get_type(configuration[j])], configuration[j], configuration[i]);
+      if(compute_on_configuration) {
+        for(size_t i(0); i < configuration_size; ++i) {
+          dispersion[i] = DispersionType(number_types);
+          for(size_t j(0); j < i; ++j) {
+            // TODO: varphi(configuration[i], configuration[j]) only needs to be computed once
+            dispersion[i][get_type(configuration[j])] += AbstractDispersion::template delta<1, true>(varphi, count_vector[j][get_type(configuration[i])], configuration[j], configuration[i]);
+            dispersion[j][get_type(configuration[i])] += AbstractDispersion::template delta<1, true>(varphi, count_vector[i][get_type(configuration[j])], configuration[j], configuration[i]);
+          }
+          add_count_to_dispersion<1, AbstractDispersion, 1>(varphi, dispersion[i], count_vector[i], configuration[i]);
         }
-        add_count_to_dispersion<1, AbstractDispersion, 1>(varphi, dispersion[i], count_vector[i], configuration[i]);
       }
 
       for(size_t i(0); i < other_configuration_size; ++i) {
-        dispersion[configuration_size + i] = DispersionType(number_types);
+        dispersion[index_configuration + i] = DispersionType(number_types);
         CountType count_point(number_types);
         for(size_t j(0); j < configuration_size; ++j) {
-          dispersion[configuration_size + i][get_type(configuration[j])] += AbstractDispersion::template delta<1, false>(varphi, count_vector[j][get_type(other_configuration[i])], configuration[j], other_configuration[i]);
+          dispersion[index_configuration + i][get_type(configuration[j])] += AbstractDispersion::template delta<1, false>(varphi, count_vector[j][get_type(other_configuration[i])], configuration[j], other_configuration[i]);
           AbstractDispersion::template update_count<0>(varphi, count_point[get_type(configuration[j])], other_configuration[i], configuration[j]);
         }
-        add_count_to_dispersion<0, AbstractDispersion, 1>(varphi, dispersion[configuration_size + i], count_point, other_configuration[i]);
+        add_count_to_dispersion<0, AbstractDispersion, 1>(varphi, dispersion[index_configuration + i], count_point, other_configuration[i]);
       }
     }
     return dispersion;
@@ -95,11 +101,12 @@ struct dispersion_computation_fitting {
 
 } // namespace detail
 
-template<typename... Configurations>
+template<bool ComputeOnConfiguration = true, typename... Configurations>
 inline auto compute_dispersion_for_fitting(const Saturated_model& model,
                                            R_xlen_t number_types,
                                            Configurations&&... configurations) {
-  return detail::dispatch_model<detail::dispersion_computation_fitting>(model, number_types, std::forward<Configurations>(configurations)...);
+  // TODO: Try to make ComputeOnConfiguration a template parameter, didn't figure out how w/ current version of dispatch
+  return detail::dispatch_model<detail::dispersion_computation_fitting>(model, number_types, ComputeOnConfiguration, std::forward<Configurations>(configurations)...);
 }
 
 } // namespace ppjsdm
