218e3a3893843bb6b301aaa7f9f8a7f42905ef22
Ian Flint <ian.flint@unimelb.edu.au>
2021-04-27 18:50:40 +1000

    Simplified call in C++ compute_papangelou

--- src/compute_papangelou.cpp
+++ src/compute_papangelou.cpp
@@ -9,18 +9,28 @@
 #include "saturated_model/exponential_family_model.hpp"
 
 // [[Rcpp::export]]
-Rcpp::NumericVector compute_papangelou_cpp(SEXP configuration, Rcpp::NumericVector x, Rcpp::NumericVector y, Rcpp::CharacterVector model, Rcpp::CharacterVector medium_range_model, Rcpp::NumericMatrix alpha, Rcpp::NumericVector beta0, Rcpp::NumericMatrix beta, Rcpp::NumericMatrix gamma, Rcpp::List covariates, Rcpp::NumericMatrix short_range, Rcpp::NumericMatrix medium_range, Rcpp::NumericMatrix long_range, R_xlen_t saturation, R_xlen_t type = 1, double mark = 1.0) {
+Rcpp::NumericVector compute_papangelou_cpp(SEXP configuration,
+                                           Rcpp::NumericVector x,
+                                           Rcpp::NumericVector y,
+                                           Rcpp::CharacterVector model,
+                                           Rcpp::CharacterVector medium_range_model,
+                                           Rcpp::NumericMatrix alpha,
+                                           Rcpp::NumericVector beta0,
+                                           Rcpp::NumericMatrix beta,
+                                           Rcpp::NumericMatrix gamma,
+                                           Rcpp::List covariates,
+                                           Rcpp::NumericMatrix short_range,
+                                           Rcpp::NumericMatrix medium_range,
+                                           Rcpp::NumericMatrix long_range,
+                                           R_xlen_t saturation,
+                                           R_xlen_t type = 1,
+                                           double mark = 1.0) {
   const ppjsdm::Configuration_wrapper wrapped_configuration(configuration);
-  const ppjsdm::Truncated_exponential_family_model<Rcpp::NumericVector> exponential_model(beta0, model, medium_range_model, alpha, beta, gamma, covariates, short_range, medium_range, long_range, saturation);
-  const auto length_x(x.size());
   std::vector<ppjsdm::Marked_point> points(x.size());
   for(typename decltype(points)::size_type i(0); i < points.size(); ++i) {
     points[i] = ppjsdm::Marked_point(x[i], y[i], type - 1, mark);
   }
-  const auto result_vector(exponential_model.compute_papangelou_vectorized(points, wrapped_configuration));
-  Rcpp::NumericVector result(Rcpp::no_init(length_x));
-  for(R_xlen_t i(0); i < length_x; ++i) {
-    result[i] = result_vector[i];
-  }
-  return result;
+
+  const ppjsdm::Truncated_exponential_family_model<Rcpp::NumericVector> exponential_model(beta0, model, medium_range_model, alpha, beta, gamma, covariates, short_range, medium_range, long_range, saturation);
+  return Rcpp::wrap(exponential_model.compute_papangelou_vectorized(points, wrapped_configuration));
 }
