c35fdd0395ee81d8469d45652b9436f8ade45ec1
Ian Flint <ian.flint@unimelb.edu.au>
2022-04-13 19:22:47 +1000

    Severe bug caused by incorrect types

--- src/prepare_gibbsm_data.cpp
+++ src/prepare_gibbsm_data.cpp
@@ -39,10 +39,10 @@ auto make_dummy_points(const ppjsdm::Window& window,
   if(dummy_distribution == std::string("binomial")) {
     D = ppjsdm::rbinomialpp_single<dummy_configuration_t>(window, rho_times_volume);
   } else if(dummy_distribution == std::string("stratified")) {
-    Vector delta(rho_times_volume);
+    std::vector<computation_t> delta(rho_times_volume.size());
     const auto volume = static_cast<computation_t>(window.volume());
-    for(auto& r: delta) {
-      r = static_cast<computation_t>(1.) / std::sqrt(static_cast<computation_t>(r) / volume);
+    for(typename decltype(delta)::size_type i(0); i < delta.size(); ++i) {
+      delta[i] = std::sqrt(volume / static_cast<computation_t>(rho_times_volume[i]));
     }
     D = ppjsdm::rstratpp_single<dummy_configuration_t>(window, delta, delta);
   } else {
