c559d5d695ebe283072bdb088814053c10bbf505
Ian Flint <iflint@2200l-154784-l.mobility.unimelb.net.au>
2022-09-12 21:39:17 +1000

    Prepare to test parallel rgibbs

--- benchmarks/rHardcore.R
+++ benchmarks/rHardcore.R
@@ -11,5 +11,5 @@ set.seed(42)
 values <- list(c(log(25), 0.15), c(log(90), 0.08), c(log(15), 0.06), c(log(55), 0.06))
 
 lapply(values, function(v) microbenchmark(
-  "ppjsdm" = rgibbs(window = window, beta0 = v[1], short_range = v[2], alpha = -Inf, gamma = 0, saturation = Inf, model = "Geyer"),
-  "spatstat" = rHardcore(exp(v[1]), R = v[2], W = window_spatstat, expand = FALSE), times = 1000L))
+  "ppjsdm" = rgibbs(window = window, beta0 = v[1], short_range = v[2], alpha = -Inf, gamma = 0, saturation = Inf, model = "Geyer", nsim = 10),
+  "spatstat" = rHardcore(exp(v[1]), R = v[2], W = window_spatstat, expand = FALSE, nsim = 10), times = 100L))
