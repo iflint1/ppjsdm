5a5f35cf74514b75b1d5a623bd7d591bd1d24e3c
Shenali Fernando <shenali5678@gmail.com>
2025-08-19 15:25:53 +1000

    update heatmap docs

--- R/heat_map.R
+++ R/heat_map.R
@@ -1,18 +1,18 @@
 #' Heatmap of coefficient values of a fit object
 #'
 #' @param fit A fit object obtained by a call to 'ppjsdm::gibbsm'
-#' @param sum Provide a summary object of the fit, only needed if show_sig = TRUE 
-#' @param coefficient A string representing the coefficient to plot 
+#' @param sum Provide a summary object of the fit, only needed if show_sig = TRUE
+#' @param coefficient A string representing the coefficient to plot, either alpha or beta
 #' @param show_values Should the cells show the coefficient value?
-#' @param show_sig Should the significance of each cell be shown by a dot in the cell? 
+#' @param show_sig Should the significance of each cell be shown by a point in the cell?
 #' @param limits Optional vector that gives what coefficient values should be plotted
-#' @param colours Colours that the gradient of the heatmap will be filled by 
+#' @param colours Colours that the gradient of the heatmap will be filled by
 #' @param scale_by For visualisation - what should the interval of legend scale increase by?
 #' @param include_y Optional vector of types that should be included on the y-axis
 #' @param include_x Optional vector of types that should be included on the x-axis
-#' @param names Optional list of full names of types, if for example abbreviations were used when running the fit 
+#' @param names Optional list of full names of types, if for example abbreviations were used when running the fit
 #' @importFrom ggplot2 aes geom_tile scale_fill_gradientn labs theme geom_text geom_point scale_size_manual
-#' @importFrom scales squish 
+#' @importFrom scales squish
 #' @importFrom rlang splice-operator
 #' @importFrom dplyr filter mutate recode
 #' @returns
@@ -32,25 +32,25 @@
 #' # Plot the heatmap of coefficients
 #'
 #' ppjsdm::heat_map(fit, coefficient = "x", show_values = FALSE)
-#' 
-heat_map <- function(fit, 
-                     summ = NULL, 
+#'
+heat_map <- function(fit,
+                     summ = NULL,
                      coefficient = c("alpha", "beta"),
-                     show_values = TRUE, 
-                     show_sig = FALSE, 
-                     limits = NULL, 
-                     colours = c("#6c63ac", "#61AEE6","#80D6BE","gray95", "#F2CE61","#F5A54F","#F55E2C"), 
+                     show_values = TRUE,
+                     show_sig = FALSE,
+                     limits = NULL,
+                     colours = c("#6c63ac", "#61AEE6","#80D6BE","gray95", "#F2CE61","#F5A54F","#F55E2C"),
                      scale_by = 0.5,
-                     include_y = NULL, 
+                     include_y = NULL,
                      include_x = NULL,
-                     names = NULL) 
+                     names = NULL)
 {
-  
+
   if(coefficient == "alpha") {
     estimates <- fit$coefficients$alpha[[1]]
   } else {
     estimates <- fit$coefficients$beta}
-  
+
 
 unique_names <- colnames(estimates)
 df <- as.data.frame(expand.grid(from = rownames(estimates), to = colnames(estimates)))
@@ -64,7 +64,7 @@ df$value <- sapply(seq_len(nrow(df)), function(i) {
 })
 
 if(!is.null(summ)){
-#insert low CI value 
+#insert low CI value
 df$lo <- sapply(seq_len(nrow(df)), function(i) { # Get the lower-bound of the CIs
   val <- sum$lo$alpha[[1]][df$from[i], df$to[i]]
   if(length(val) == 0) {
@@ -73,7 +73,7 @@ df$lo <- sapply(seq_len(nrow(df)), function(i) { # Get the lower-bound of the CI
   val
 })
 
-#insert high CI value 
+#insert high CI value
 df$hi <- sapply(seq_len(nrow(df)), function(i) { # Get the lower-bound of the CIs
   val <- sum$hi$alpha[[1]][df$from[i], df$to[i]]
   if(length(val) == 0) {
@@ -83,7 +83,7 @@ df$hi <- sapply(seq_len(nrow(df)), function(i) { # Get the lower-bound of the CI
 })
 
 df$sig <- sapply(seq_len(nrow(df)), function(i) {
-  sig <- ifelse(df$lo[i] > 0 | df$hi[i] < 0, "1", "0") #make significance column, 1 = significant, 0 = not significant 
+  sig <- ifelse(df$lo[i] > 0 | df$hi[i] < 0, "1", "0") #make significance column, 1 = significant, 0 = not significant
 sig
   })
 
@@ -91,24 +91,24 @@ sig
 
 
 if(!is.null(include_x)){
-  
+
   df <- df %>% filter(from %in% include_x)
-  
+
 }
 
 if(!is.null(include_y)){
-  
+
   df <- df %>% filter(to %in% include_y)
-  
+
 }
 
 if(!is.null(names)){
   df <- df %>% mutate(from = recode(from, !!!names))
-                                    
-  df <- df %>% mutate(to = recode(to, !!!names))                             
+
+  df <- df %>% mutate(to = recode(to, !!!names))
 }
 
-  
+
 if(!is.null(limits)){
 breaks <- seq(limits[1], limits[2], by = scale_by)
 
@@ -119,66 +119,66 @@ custom_labels <- c(paste0(">", limits[1]), breaks[2:(length(breaks) - 1)], paste
 }
 
 
-p <- ggplot(df, aes(x = from, 
-                    y = to, 
+p <- ggplot(df, aes(x = from,
+                    y = to,
                     fill = value)) +
- 
-     geom_tile(colour = "white", 
-               height = 1.0, 
-               alpha = 0.75 ) + 
-    
+
+     geom_tile(colour = "white",
+               height = 1.0,
+               alpha = 0.75 ) +
+
      scale_fill_gradientn(colours = colours,
-                          limits = limits, 
-                          breaks = breaks, 
-                          labels = custom_labels, 
-                          name = "Coefficient", 
+                          limits = limits,
+                          breaks = breaks,
+                          labels = custom_labels,
+                          name = "Coefficient",
                           oob = squish) +
-     
-     labs(x = "", 
-          y = "") + 
-       
+
+     labs(x = "",
+          y = "") +
+
     theme(panel.grid.major = element_blank(),
-                        
+
           panel.grid.minor = element_blank(),
-                       
+
           panel.background = element_blank(),
-              
+
          legend.position = "right",
-                      
+
          legend.title=element_text(size=15),
-                       
+
           legend.text=element_text(size=12),
-              
+
          axis.title.x = element_text(size=25),
-                 
+
           axis.title.y = element_text(size=25),
-              
+
           axis.text.x = element_text(size=15, angle = 45, hjust = 1, face = "bold"),
-                        
+
         axis.text.y = element_text(size=15, face = "bold"),
-                       
+
           strip.text.y = element_text(size = 15),
-              
-          strip.text.x = element_text(size = 15)) 
+
+          strip.text.x = element_text(size = 15))
 
 
-  
+
 
 if(show_values == TRUE){
   p <- p + geom_text(aes(label = round(value, 2)), color = "black", size = 4.25)
-  }  
+  }
 
 if(show_sig == TRUE) {
-  
-df$sig <- factor(df$sig) 
 
-  p <- p + 
+df$sig <- factor(df$sig)
+
+  p <- p +
     geom_point(aes(size = as.factor(sig))) +
     scale_size_manual(values = c(NA, 2),
                       name = " ",
-                      breaks = c("", "1"), 
-                      labels = c("", "Significant")) 
-  
+                      breaks = c("", "1"),
+                      labels = c("", "Significant"))
+
   }
 
  print(p)
